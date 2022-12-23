6c3e2d0a02d0792b1675c76b0bddc857d8ddc35e
pys5205 <skypark5205@naver.com>
2022-12-23 11:26:24 +0900

    disk_network kafka

--- agent/disk_network.py
+++ agent/disk_network.py
@@ -20,13 +20,9 @@ with open('disk_network.yaml') as f:
     data = yaml.load(f)
 # disk_list = ["/", "/www", "/data"]
 
+#main info
 main_info = {}
-main_info["system"] = "system"
-main_info["service"] = "service"
-main_info["company"] = "company"
-main_info["os"] = "os"
-main_info["ts_db_insert"] = "ts_db_insert"
-main_info["ts_db_create"] = "ts_db_create"
+main_info["user"] = data.get("main_list")
 
 while(1):
     # DISK 정보
@@ -40,14 +36,12 @@ while(1):
             if p.mountpoint in data.get("disk_list"):
                 du = psutil.disk_usage(p.mountpoint)
                 disk_part_info = {"device":p.device, "mountpoint":p.mountpoint, "fstype":p.fstype,"opts":p.opts,"total":(du.total),"used":(du.used),"free":(du.free), "percent":du.percent}
+                disk_info["info"].append(disk_part_info)
             # print(disk_part_info)
         except:
             pass
     # disk io counters 정보
     disk_io_info = psutil.disk_io_counters(perdisk=True)
-
-    # disk info append
-    disk_info["info"].append(disk_part_info)
     disk_info["info"].append(disk_io_info)
 
     # kafka 사용
@@ -66,12 +60,13 @@ while(1):
 
     # net io counters 정보
     net_io_info = psutil.net_io_counters(pernic=True)
-
+    net_info["info"].append(net_io_info)
     # net if addrs 정보
     net_if = psutil.net_if_addrs()
     for name, addrs in net_if.items():
         for addr in addrs:
             net_if_info = {"family":addr.family, "address":addr.address, "netmask":addr.netmask, "broadcast":addr.broadcast, "ptp":addr.ptp}
+            net_info["info"].append(net_if_info)
             #print(net_if_info)
 
     # net connections 정보
@@ -80,14 +75,10 @@ while(1):
         try:
             if x.status == 'ESTABLISHED':
                 net_con_info = {"fd":x.fd, "family":x.family, "type":x.type, "laddr":x.laddr, "raddr":x.raddr, "status":x.status, "pid":x.pid}
+                net_info["info"].append(net_con_info)
                 #print(net_con_info)
         except:
             pass
-
-    # net info append
-    net_info["info"].append(net_io_info)
-    net_info["info"].append(net_if_info)
-    net_info["info"].append(net_con_info)
 
     producer.send(
         "test",
