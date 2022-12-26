8263c1cbe8a129b30428ee403cb654bf9b69a49f
jaechul <chel8780@gmail.com>
2022-12-26 20:10:43 +0900

    data file update

--- agent/data.py
+++ agent/data.py
@@ -29,32 +29,26 @@ main_info["user"] = data.get("main_list")
 
 while(1):
     cpu_info = main_info
-
     cpu_info["type"]= "cpu"
     cpu_info["info"]=[]
     #cpu 정보
     cpuTime = psutil.cpu_times()
 
-    # cpus["cpu_sys"] = cpuTime.system
-    # cpus["cpu_user"] = cpuTime.user
-    # cpus["cpu_wait"] = cpuTime.iowait
-    # cpus["cpu_irq"] = cpuTime.irq
-    # cpus["cpu_softirq"] = cpuTime.softirq
-
-    cpuCount = {"cpu_count":psutil.cpu_count(logical=False)}
+    cpuCount = psutil.cpu_count(logical=False)
     # cpu_info["cpu_count"] = cpuCount
 
     cpuFreq = psutil.cpu_freq(percpu=True)
     for freq in cpuFreq :
           cpu_frq = {"cpu_frq":{"current":freq.current, "min":freq.min, "max":freq.max}}
-          cpu_info["info"].append(cpu_frq)
+          print(cpu_frq)
+          # cpu_info["info"].append(cpu_frq)
 
     # cpus["cpu_frq"] = cpu_frq
     cpuLoadavg = psutil.getloadavg()
-    cpus = {"cpu_sys": cpuTime.system,"cpu_user":cpuTime.user,"cpu_wait":cpuTime.iowait, "cpu_irq":cpuTime.irq, "cpu_softirq":cpuTime.softirq,"cpu_count":cpuCount, "cpu_loadavg": cpuLoadavg}
+    cpus = {"cpu_sys": cpuTime.system,"cpu_user":cpuTime.user,"cpu_wait":cpuTime.iowait, "cpu_irq":cpuTime.irq, "cpu_softirq":cpuTime.softirq, "cpu_count":cpuCount, "cpu_loadavg": cpuLoadavg}
     cpu_info["info"].append(cpus)
 
-    # kafka 사용
+    kafka 사용
     producer.send(
         "test",
         key = hostname,
@@ -70,19 +64,7 @@ while(1):
     mem = psutil.virtual_memory()
     memSwap = psutil.swap_memory()
 
-    # mem_free["mem_total"] = mem.total
-    # mem_free["mem_avail"] = mem.available
-    # mem_free["mem_used"] = mem.used
-    # mem_free["mem_free"] = mem.free
-    # mem_free["mem_buffer"] = mem.buffers
-    # mem_free["mem_cached"] = mem.cached
-    # mem_free["mem_shared"] = mem.shared
-    # mem_free["memSwap_total"] = memSwap.total
-    # mem_free["memSwap_used"] = memSwap.used
-    # mem_free["memSwap_free"] = memSwap.free
-    # mem_free["memSwap_percent"] = memSwap.percent
-    # mem_free["memSwap_sin"] = memSwap.sin
-    # mem_free["memSwap_sout"] = memSwap.sout
+
     mem_in={"mem_total": mem.total, "mem_avail": mem.available,"mem_used":mem.used,"mem_free":mem.free,"mem_buffer":mem.buffers,"mem_cached":mem.cached,"mem_shared":mem.shared,"memSwap_total":memSwap.total,"memSwap_used":memSwap.used,"memSwap_free":memSwap.free,"memSwap_percent":memSwap.percent,"memSwap_sin":memSwap.sin,"memSwap_sout":memSwap.sout}
     mem_info["info"].append(mem_in)
     # print(mem_info)
@@ -99,34 +81,42 @@ while(1):
     procs_info = main_info
     procs_info["type"]="process"
     procs_info["info"]=[]
-    procs = psutil.Process()
-    procsIter = psutil.process_iter(['pid', 'name', 'username'])
-    for p in procsIter:
-        procs_info["info"].append(p.info)
-    # procs_iter = {p.pid: p.info for p in psutil.process_iter(['name', 'username'])}
-    # procss["procs_name"] = procs.name()
-    # procss["procs_ppid"] = procs.ppid()
 
+    procs = psutil.Process()
+    # with procs.oneshot():
+    #     print(procs.name())
+    # with procs.oneshot():
+    #     for p in procs:
+    #         print(p.name())
+    # for t in procs:
+    #     print(t.name())
+    # procsIter = psutil.process_iter(['pid', 'name', 'username','ppid','cpu_times','memory_full_info','memory_maps','cmdline'])
+    # procsIter = psutil.process_iter()
     procs_cpuTimes = procs.cpu_times()
-    # procss["procs_cpuT_user"] = procs_cpuTimes.user
-    # procss["procs_cpuT_sys"] = procs_cpuTimes.system
-    # procss["procs_cpuT_children_user"] = procs_cpuTimes.children_user
-    # procss["procs_cpuT_children_sys"] = procs_cpuTimes.children_system
-    # procss["procs_cpuT_iowait"] = procs_cpuTimes.iowait
-    #
-    # procss["procs_percent"] = procs.cpu_percent()
-    # procss["procs_createTime"] = procs.create_time()
-    # procss["procs_status"] = procs.status()
-    # procss["procs_terminal"] = procs.terminal()
-    # procss["procs_numTreads"] = procs.num_threads()
     procsMem = procs.memory_full_info()
     procsMemMaps = procs.memory_maps()
-    doc_procs = {"procs_name":procs.name(),"procs_ppid":procs.ppid(),"procs_cpuT_user":procs_cpuTimes.user,"procs_cpuT_sys":procs_cpuTimes.system,"procs_cpuT_children_user":procs_cpuTimes.children_user,"procs_cpuT_children_sys":procs_cpuTimes.children_system,"procs_cpuT_iowait":procs_cpuTimes.iowait,"procs_percent":procs.cpu_percent(),"procs_createTime":procs.create_time(),"procs_status":procs.status(),"procs_terminal":procs.terminal(),"procs_numTreads":procs.num_threads(),"procs_mem_full_uss":procsMem.uss,"procs_mem_full_pss":procsMem.pss,"procs_mem_full_swap":procsMem.swap}
-    procs_info["info"].append(doc_procs)
 
     for pmm in procsMemMaps :
         doc_procs_mm = {"path":pmm.path,"rss":pmm.rss,"size":pmm.size,"pss":pmm.pss,"shared_clean":pmm.shared_clean,"private_dirty":pmm.private_dirty,"referenced":pmm.referenced,"anonymous":pmm.anonymous}
         procs_info["info"].append(doc_procs_mm)
+
+    for proc in psutil.process_iter():
+        if proc.name() in data.get('procs_list'):
+            doc_procs = {"procs_name":proc.name(),"procs_pid":proc.pid,"procs_cpuT_user":procs_cpuTimes.user,"procs_cpuT_sys":procs_cpuTimes.system,"procs_cpuT_children_user":procs_cpuTimes.children_user,"procs_cpuT_children_sys":procs_cpuTimes.children_system,"procs_cpuT_iowait":procs_cpuTimes.iowait,"procs_percent":procs.cpu_percent(),"procs_createTime":procs.create_time(),"procs_status":procs.status(),"procs_terminal":procs.terminal(),"procs_numTreads":procs.num_threads(),"procs_mem_full_uss":procsMem.uss,"procs_mem_full_pss":procsMem.pss,"procs_mem_full_swap":procsMem.swap,"procs_cmdline":procs.cmdline()}
+            procs_info["info"].append(doc_procs)
+            # print(proc.name(), proc.pid, proc.username(), procs_cpuTimes.user, procs_cpuTimes.system, procs_cpuTimes.children_user, procs_cpuTimes.children_system, procs_cpuTimes.iowait, procs.cpu_percent(),procs.create_time(),procs.status(), procs.terminal(), procs.num_threads(), procsMem.uss, procsMem.pss, procsMem.swap,procs.cmdline())
+
+            # print(p.info)
+    # pptt = procs.as_dict(['pid', 'name', 'username'])
+    # print(pptt)
+    # for a in pptt.items() :
+    #     print(a)
+    # for p in pptt :
+    #     print(p)
+    # doc_procs = {"procs_cpuT_user":procs_cpuTimes.user,"procs_cpuT_sys":procs_cpuTimes.system,"procs_cpuT_children_user":procs_cpuTimes.children_user,"procs_cpuT_children_sys":procs_cpuTimes.children_system,"procs_cpuT_iowait":procs_cpuTimes.iowait,"procs_percent":procs.cpu_percent(),"procs_createTime":procs.create_time(),"procs_status":procs.status(),"procs_terminal":procs.terminal(),"procs_numTreads":procs.num_threads(),"procs_mem_full_uss":procsMem.uss,"procs_mem_full_pss":procsMem.pss,"procs_mem_full_swap":procsMem.swap,"procs_cmdline":procs.cmdline()}
+        # procs_info["info"].append(doc_procs)
+
+
 
     # print(procs_info)
 
