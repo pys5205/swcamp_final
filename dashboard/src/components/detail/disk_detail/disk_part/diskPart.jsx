2fb32a248189828a8eb579f9466babdfbfe75556
pys5205 <skypark5205@naver.com>
2023-01-18 17:45:20 +0900

    detail_disk,network

---
+++ dashboard/src/components/detail/disk_detail/disk_part/diskPart.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import Chart from "react-apexcharts";
+import './diskPart.css'
+
+export default class diskpart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+  
+ componentDidMount(){
+   const current = decodeURI(window.location.href);
+   // console.log(current.split('/')[4])
+   const system = current.split('/')[4];
+  fetch("/memory", { 
+      method: "post", //통신방법
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        'system':system
+      }),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+       if (json === undefined) {
+                    alert("오류");
+                  } else {
+                  //////////////////////////////////여기부터보자
+                  // console.log(json);
+                    this.setState({
+                      isLoaded: true,
+                     data : json
+                    })
+                  }
+      });
+  }
+  render() {
+    // console.log(this.state.data);
+    const Data = this.state.data;
+    const test = Data.server_memory;
+    // console.log(test);
+    return(
+      <div className="app">
+        <div className="row">
+          <div className="donut">
+            <Chart
+             type="pie"
+             height="250"
+              series={test } 
+            options={{
+                labels: ['사용중', '빈공간', '버퍼', '캐시']
+            }}
+            />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
